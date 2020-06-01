b429269b997e06dfc47067daefb6feeb622e5056
Roger Campbell II <rc1336@gmail.com>
2020-05-31 20:53:48 -0400

    Moved user model out of authService and into DB directory

--- authService/routes/user.js
+++ authService/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const User = require('../models/User')
+const User = require('../../db/models/User')
 const auth = require('../middleware/auth')
 
 const router = express.Router()
